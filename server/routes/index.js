f4200b78f3c18346c54a0876d219ca765727b40c
Angham116 <angham.1161995@hotmail.com>
2019-11-20 20:43:30 +0200

    signup controller
    #6

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,11 @@
 const boom = require('@hapi/boom');
 const express = require('express');
 
+const signup = require('./signup');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({ body: 'test route' });
-});
+router.post('/signup', signup);
 
 // catch 404 and forward to error handler
 router.use((req, res, next) => {
---
+++ server/routes/signup/index.js
@@ -0,0 +1,33 @@
+const signupSchema = require('./utils/validation');
+const insertNewUser = require('../../database/queries/createUser');
+const hashPassword = require('./utils/hashPassword');
+
+module.exports = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const valid = await signupSchema
+      .validate({
+        username,
+        email,
+        password,
+      }, { abortEarly: false });
+    if (valid) {
+      const hashedPassword = await hashPassword(password);
+      const newUser = await insertNewUser({ username, email, password: hashedPassword });
+      if (newUser) {
+        res.cookie('jwt', { username }, { maxAge: 30 * 2 * 24 * 60 * 60 * 10000 }, { HttpOnly: true });
+        res.status(200).send({ data: { username: newUser.username }, err: null });
+      } else {
+        throw Error('validation error');
+      }
+    }
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(422).send({ data: null, err: error.errors });
+    } else if (error.code === 11000) {
+      res.status(401).send({ data: null, err: ['Username or email is already exist'] });
+    } else {
+      res.status(500).send({ data: null, err: 'Internal Server Error' });
+    }
+  }
+};
