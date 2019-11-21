ed6f7c8a5b80226c05b49680cc2ac8b493818ad3
Angham116 <angham.1161995@hotmail.com>
2019-11-21 13:50:54 +0200

    refactor shared btn component
    #10

--- client/src/components/Button/index.jsx
+++ client/src/components/Button/index.jsx
@@ -5,21 +5,17 @@ import { Button } from 'antd';
 import './index.css';
 
 function SharedButton(props) {
-  const { text } = props;
+  const { children } = props;
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <Button {...props} className="btn" size="large">
-      {text}
+      {children}
     </Button>
   );
 }
 
 export default SharedButton;
 
-SharedButton.defaultProps = {
-  text: '',
-};
-
 SharedButton.propTypes = {
-  text: PropTypes.string,
+  children: PropTypes.node.isRequired,
 };
