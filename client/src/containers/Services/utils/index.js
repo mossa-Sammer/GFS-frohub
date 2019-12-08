94cebe2fb5bf13587cf2322746bc9137e8aa9b27
Ahmed A. Shatat <a.shatat@hotmail.com>
2019-12-08 15:54:57 +0200

    filter services by treatment + tests and mock data
    #24

--- client/src/containers/Services/utils/index.js
+++ client/src/containers/Services/utils/index.js
@@ -1,5 +1,11 @@
 // eslint-disable-next-line no-unused-vars, import/prefer-default-export
 export const filterServices = (services, fields) => {
-  // filter
-  return services;
+  const { treatment, location, date, time } = fields;
+
+  if (!treatment && !location && !date && !time) return services;
+  const filteredServices = services.filter(service => {
+    if (!treatment) return true;
+    return service.categories[0].id === Number(treatment);
+  });
+  return filteredServices;
 };
