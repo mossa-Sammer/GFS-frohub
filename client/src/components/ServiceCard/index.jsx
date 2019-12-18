50b4b1002291d42c73990d79fb297ae7beb3deb5
Angham116 <angham.1161995@hotmail.com>
2019-12-18 12:38:25 +0200

    refactor services cards style

--- client/src/components/ServiceCard/index.jsx
+++ client/src/components/ServiceCard/index.jsx
@@ -1,42 +1,31 @@
 import React, { Component } from 'react';
-
 import { Card } from 'antd';
-
 import './style.css';
 
 export default class ServiceCard extends Component {
   render() {
     const { data } = this.props;
+    const imgSrc = data.images.length
+      ? data.images[0].src
+      : 'https://s3-eu-west-2.amazonaws.com/frohub-content/wp-content/uploads/2019/05/09151932/woocommerce-placeholder-300x300.png';
     return (
       <Card
         className="service__card"
         cover={
-          data.images.length ? (
-            <img
-              src={data.images[0].src}
-              alt="frohub service"
-              className="service__img"
-            />
-          ) : (
-            <img
-              src="https://s3-eu-west-2.amazonaws.com/frohub-content/wp-content/uploads/2019/05/09151932/woocommerce-placeholder-300x300.png"
-              alt="frohub service"
-              className="service__img defautl-img"
-            />
-          )
+          <img src={imgSrc} alt="frohub service" className="service__img" />
         }
       >
         <h2 className="service__title">{data.name}</h2>
-        <span className="price">
-          £{Number.parseFloat(data.display_cost).toFixed(2)}
+        <span className="service__price">
+          £ {Number.parseFloat(data.display_cost).toFixed(2)}
         </span>
         <a
-          className="service-link"
+          className="service__link"
           href={data.permalink}
           rel="noopener noreferrer"
           target="_blank"
         >
-          See more
+          <span>See more</span>
         </a>
       </Card>
     );
