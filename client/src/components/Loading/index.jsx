68f835769f1cf14c6a7f2c9b327ef8aeba4e0ca6
Angham116 <angham.1161995@hotmail.com>
2019-12-18 11:00:02 +0200

    style loading component
    #41

--- client/src/components/Loading/index.jsx
+++ client/src/components/Loading/index.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { Spin } from 'antd';
+import { Spin, Icon } from 'antd';
+
+import './style.css';
 
 export default class Loading extends Component {
   render() {
-    return <Spin />;
+    return (
+      <div className="loading__container">
+        <Spin indicator={<Icon className="loading__icon" type="loading" />} />
+        <span className="loading__text">Loading</span>
+      </div>
+    );
   }
 }
