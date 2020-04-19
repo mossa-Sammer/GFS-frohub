7d6620e9e302b92ae8f9575371ddaf01590ab4ad
Angham116 <angham.1161995@hotmail.com>
2020-04-19 15:14:10 +0300

    add config vars into config file

--- server/config/config.js
+++ server/config/config.js
@@ -3,11 +3,14 @@ require('dotenv').config();
 
 // Get Mongodb URI
 let mongoURI = process.env.MONGO_URI_DEV;
+let DATABASE_URL = process.env.DATABASE_URL_DEV;
 
 if (process.env.NODE_ENV === 'test') {
   mongoURI = process.env.MONGOURI_TEST;
+  DATABASE_URL = process.env.TEST_DB;
 } else if (process.env.NODE_ENV === 'production') {
   mongoURI = process.env.MONGO_URI;
+  DATABASE_URL = process.env.DATABASE_URL;
 }
 
 const secret = process.env.SECRET;
@@ -15,4 +18,5 @@ const secret = process.env.SECRET;
 module.exports = {
   mongoURI,
   secret,
+  DATABASE_URL,
 };
