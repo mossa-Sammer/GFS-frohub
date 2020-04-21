fed1df1785bf6b07a187324bc66871eca63019b2
Angham116 <angham.1161995@hotmail.com>
2020-04-21 11:03:50 +0300

    solve commit suggestion

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 // Get Mongodb URI
 let mongoURI = process.env.MONGO_URI_DEV;
-let DATABASE_URL = process.env.DATABASE_URL_DEV;
+let DATABASE_URL = '';
 
 if (process.env.NODE_ENV === 'test') {
   mongoURI = process.env.MONGOURI_TEST;
@@ -11,6 +11,8 @@ if (process.env.NODE_ENV === 'test') {
 } else if (process.env.NODE_ENV === 'production') {
   mongoURI = process.env.MONGO_URI;
   DATABASE_URL = process.env.DATABASE_URL;
+} else {
+  DATABASE_URL = process.env.DATABASE_URL_DEV;
 }
 
 const secret = process.env.SECRET;
