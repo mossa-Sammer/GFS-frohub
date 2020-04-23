cc269de6dbe27fb44c32494cae5bf99a74b43f05
mossa-Sammer <mossa708496@gmail.com>
2020-04-23 20:14:14 +0300

    tested the get user db query relates #122

--- server/__tests__/database/getUser.test.js
+++ server/__tests__/database/getUser.test.js
@@ -1,12 +1,43 @@
 const dbConnection = require('../../database/config/dbConnection');
 const dbBuild = require('../../database/config/dbBuild');
 
+const { getUserByEmail, getUserById } = require('../../database/sql_queries');
+
 
 beforeAll(() => dbBuild());
 
 afterAll(() => dbConnection.end());
 
-test('get user without existing id', () => {});
-test('get user by id', () => {});
-test('get user without existing email', () => {});
-test('get user by email', () => {});
+describe('get user by email or id', () => {
+  test('get user without existing id', () => {
+    expect.assertions(1);
+    return getUserById(12342134).then((data) => {
+      const { rows } = data;
+      expect(rows[0]).toBeUndefined();
+    });
+  });
+
+  test('get user by id', () => {
+    expect.assertions(1);
+    return getUserById(1).then((data) => {
+      const { rows } = data;
+      expect(rows[0]).toBeDefined();
+    });
+  });
+
+  test('get user without existing email', () => {
+    expect.assertions(1);
+    return getUserByEmail('skidrow@gmail.com').then((data) => {
+      const { rows } = data;
+      expect(rows[0]).toBeUndefined();
+    });
+  });
+
+  test('get user by email', () => {
+    expect.assertions(1);
+    return getUserByEmail('mossa@gmail.com').then((data) => {
+      const { rows } = data;
+      expect(rows[0]).toBeDefined();
+    });
+  });
+});
