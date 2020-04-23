f7e8bdd8ba327730e3081053db6a2cda0a257af0
mossa-Sammer <mossa708496@gmail.com>
2020-04-23 20:12:45 +0300

    removed mongodb test url relates #122

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ let mongoURI = process.env.MONGO_URI_DEV;
 let DATABASE_URL = '';
 
 if (process.env.NODE_ENV === 'test') {
-  mongoURI = process.env.MONGOURI_TEST;
+  // mongoURI = process.env.MONGOURI_TEST;
   DATABASE_URL = process.env.TEST_DB;
 } else if (process.env.NODE_ENV === 'production') {
   mongoURI = process.env.MONGO_URI;
