82fb091b1d14b83ca3fa6bdcb094415093d993ee
mossa-Sammer <mossa708496@gmail.com>
2020-04-26 13:40:08 +0300

    removed comment relates #122

--- server/controllers/stylist/personal/getStylist.js
+++ server/controllers/stylist/personal/getStylist.js
@@ -1,6 +1,5 @@
 const { getUserById } = require('../../../database/sql_queries');
 
-// eslint-disable-next-line no-unused-vars
 module.exports = (req, res, next) => {
   const { id } = req.params;
   getUserById(id).then((data) => {
