b0917ee814ab1d006cb10cc250d9513227388874
mossa-Sammer <mossa708496@gmail.com>
2020-04-26 13:38:08 +0300

    tested the returned keys relates #122

--- server/__tests__/api/getUser.test.js
+++ server/__tests__/api/getUser.test.js
@@ -4,7 +4,25 @@ const dbConnection = require('../../database/config/dbConnection');
 
 afterAll(() => dbConnection.end());
 
-test('get the user using id in query param', (done) => request(app)
-  .get('/api/stylist/1')
-  .expect('Content-Type', /json/)
-  .expect(200, done));
+test('get the user using id in query param', (done) => {
+  const expectedFields = [
+    'user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role',
+  ];
+  expectedFields.sort();
+
+  expect.assertions(expectedFields.length);
+
+  request(app)
+    .get('/api/stylist/1')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end((err, res) => {
+      if (err) return done(err);
+      const fields = Object.keys(res.body).sort();
+      for (let i = 0; i < expectedFields.length; i += 1) {
+        expect(expectedFields[i]).toBe(fields[i]);
+      }
+
+      return done();
+    });
+});
