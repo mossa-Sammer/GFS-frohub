1cbfbac497f00cf7ad43a6e1666720d93acc297c
mossa-Sammer <mossa708496@gmail.com>
2020-04-26 12:56:02 +0300

    used dynamic data in testing relates #122

--- server/__tests__/database/getUser.test.js
+++ server/__tests__/database/getUser.test.js
@@ -9,35 +9,25 @@ beforeAll(() => dbBuild());
 afterAll(() => dbConnection.end());
 
 describe('get user by email or id', () => {
-  test('get user without existing id', () => {
-    expect.assertions(1);
-    return getUserById(12342134).then((data) => {
-      const { rows } = data;
-      expect(rows[0]).toBeUndefined();
-    });
-  });
-
   test('get user by id', () => {
     expect.assertions(1);
-    return getUserById(1).then((data) => {
-      const { rows } = data;
-      expect(rows[0]).toBeDefined();
-    });
-  });
-
-  test('get user without existing email', () => {
-    expect.assertions(1);
-    return getUserByEmail('skidrow@gmail.com').then((data) => {
-      const { rows } = data;
-      expect(rows[0]).toBeUndefined();
+    return dbConnection.query('SELECT * FROM "user"').then((result) => {
+      const { rows: [user] } = result;
+      return getUserById(user.user_id);
+    }).then((result) => {
+      const { rows: [user] } = result;
+      expect(user).toBeDefined();
     });
   });
 
   test('get user by email', () => {
     expect.assertions(1);
-    return getUserByEmail('mossa@gmail.com').then((data) => {
-      const { rows } = data;
-      expect(rows[0]).toBeDefined();
+    return dbConnection.query('SELECT * FROM "user"').then((result) => {
+      const { rows: [user] } = result;
+      return getUserByEmail(user.email);
+    }).then((result) => {
+      const { rows: [user] } = result;
+      expect(user).toBeDefined();
     });
   });
 });
