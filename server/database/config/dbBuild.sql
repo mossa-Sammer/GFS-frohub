712278207163c6f8e5f041b1139ae740c29d5322
Angham116 <angham.1161995@hotmail.com>
2020-04-26 16:13:12 +0300

    add sort_code field to business table #125

--- server/database/config/dbBuild.sql
+++ server/database/config/dbBuild.sql
@@ -24,6 +24,7 @@ CREATE TABLE business (
   user_id INT REFERENCES "user"(user_id) on DELETE CASCADE,
   full_name VARCHAR(255),
   account_number  VARCHAR(12) UNIQUE,
+  sort_code VARCHAR(12) UNIQUE,
   preferred_pay_method pay_method
 );
 
