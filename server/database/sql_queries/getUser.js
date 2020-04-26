176fee4a1197f63ef7dd86d681925ca5acffa132
mossa-Sammer <mossa708496@gmail.com>
2020-04-26 13:39:02 +0300

    refactor queries relates #122

--- server/database/sql_queries/getUser.js
+++ server/database/sql_queries/getUser.js
@@ -1,17 +1,5 @@
 const connection = require('../config/dbConnection');
 
-exports.getUserById = (id) => {
-  const sql = {
-    text: 'SELECT * FROM "user" WHERE user_id=$1',
-    values: [id],
-  };
-  return connection.query(sql);
-};
+exports.getUserById = (id) => connection.query('SELECT * FROM "user" WHERE user_id=$1', [id]);
 
-exports.getUserByEmail = (email) => {
-  const sql = {
-    text: 'SELECT * FROM "user" WHERE email=$1',
-    values: [email],
-  };
-  return connection.query(sql);
-};
+exports.getUserByEmail = (email) => connection.query('SELECT * FROM "user" WHERE email=$1', [email]);
