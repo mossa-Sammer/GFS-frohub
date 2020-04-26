405d2d26d9fdb5c0f9beb03f2cb97c6086e3df40
mossa-Sammer <mossa708496@gmail.com>
2020-04-26 13:52:03 +0300

    removed sorting relates #122

--- server/__tests__/api/getUser.test.js
+++ server/__tests__/api/getUser.test.js
@@ -5,12 +5,11 @@ const dbConnection = require('../../database/config/dbConnection');
 afterAll(() => dbConnection.end());
 
 test('get the user using id in query param', (done) => {
+  expect.assertions(1);
   const expectedFields = [
     'user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role',
   ];
-  expectedFields.sort();
 
-  expect.assertions(expectedFields.length);
 
   request(app)
     .get('/api/stylist/1')
@@ -18,10 +17,8 @@ test('get the user using id in query param', (done) => {
     .expect(200)
     .end((err, res) => {
       if (err) return done(err);
-      const fields = Object.keys(res.body).sort();
-      for (let i = 0; i < expectedFields.length; i += 1) {
-        expect(expectedFields[i]).toBe(fields[i]);
-      }
+      const fields = Object.keys(res.body);
+      expect(expectedFields).toEqual(fields);
 
       return done();
     });
