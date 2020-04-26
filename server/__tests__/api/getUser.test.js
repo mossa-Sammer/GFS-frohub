08f19f98002c15ab936acf3d858bdc4a8c98329d
mossa-Sammer <mossa708496@gmail.com>
2020-04-26 14:01:07 +0300

    used dynamic data relates #122

--- server/__tests__/api/getUser.test.js
+++ server/__tests__/api/getUser.test.js
@@ -10,16 +10,22 @@ test('get the user using id in query param', (done) => {
     'user_id', 'first_name', 'last_name', 'email', 'phone_number', 'role',
   ];
 
+  dbConnection.query('SELECT * FROM "user" LIMIT 1')
+    .then((data) => {
+      const {
+        rows: [user],
+      } = data;
 
-  request(app)
-    .get('/api/stylist/1')
-    .expect('Content-Type', /json/)
-    .expect(200)
-    .end((err, res) => {
-      if (err) return done(err);
-      const fields = Object.keys(res.body);
-      expect(expectedFields).toEqual(fields);
+      request(app)
+        .get(`/api/stylist/${user.user_id}`)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          const fields = Object.keys(res.body);
+          expect(expectedFields).toEqual(fields);
 
-      return done();
+          return done();
+        });
     });
 });
