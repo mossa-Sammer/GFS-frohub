f14888e332aeae85fe40ebd3b10d378f481edfe0
mossa-Sammer <mossa708496@gmail.com>
2020-04-26 15:00:01 +0300

    tested the query relates #123

--- server/__tests__/database/addPersonalData.js
+++ server/__tests__/database/addPersonalData.js
@@ -1,20 +1,22 @@
 const dbConnection = require('../../database/config/dbConnection');
 const dbBuild = require('../../database/config/dbBuild');
-const { addUser } = require('../../database/sql_queries');
+const { addPersonalData } = require('../../database/sql_queries');
 
 beforeAll(() => dbBuild());
 afterAll(() => dbConnection.end());
 
 
 test('add a stylist', () => {
-  expect.assertions(1);
+  expect.assertions(2);
   const data = {
     firstName: 'soha',
     lastName: 'kadi',
     email: 'soha@gmail.com',
     phone: '1231244',
   };
-  return addUser(data).then(({ rows }) => {
-    expect(rows[0].role).toBe('stylist');
+  return addPersonalData(data).then((result) => {
+    const { rows: [user] } = result;
+    expect(user.email).toBe(data.email);
+    expect(user.role).toBe('stylist');
   });
 });
