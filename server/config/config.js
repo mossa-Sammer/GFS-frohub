1d7eac5bcba96acd1d71ffe30b039a6d62f8dad4
mossa-Sammer <mossa708496@gmail.com>
2020-04-26 14:08:19 +0300

    removed unused mongo uri relates #122

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,6 @@ let mongoURI = process.env.MONGO_URI_DEV;
 let DATABASE_URL = '';
 
 if (process.env.NODE_ENV === 'test') {
-  // mongoURI = process.env.MONGOURI_TEST;
   DATABASE_URL = process.env.TEST_DB;
 } else if (process.env.NODE_ENV === 'production') {
   mongoURI = process.env.MONGO_URI;
