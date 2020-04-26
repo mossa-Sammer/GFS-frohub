9f17b3fb08e9e71b49aa59701cc1ff24af496648
Angham116 <angham.1161995@hotmail.com>
2020-04-26 16:18:35 +0300

    post stylist business details api test /api/stylist/:id/business #125

--- server/controllers/stylist/business/addStylistBusiness.js
+++ server/controllers/stylist/business/addStylistBusiness.js
@@ -1,3 +1,38 @@
-const { notImplemented } = require('@hapi/boom');
-// eslint-disable-next-line no-unused-vars
-module.exports = (req, res, next) => next(notImplemented('not implemented'));
+const { unauthorized } = require('@hapi/boom');
+const {
+  insertStylistBusiness,
+  checkStylist,
+} = require('../../../database/queries');
+
+const { validateAddBusiness } = require('./validation/addBusiness');
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+  const {
+    fullName,
+    accountNum,
+    sortCode,
+    preffaredPayMethod,
+  } = req.body;
+  const isStylist = checkStylist(id);
+  validateAddBusiness.validate({
+    fullName,
+    accountNum,
+    sortCode,
+  }).then(() => {
+    isStylist.then((result) => {
+      if (result.rows.length) {
+        insertStylistBusiness(id, {
+          fullName, accountNum, sortCode, preffaredPayMethod,
+        })
+          .then((business) => {
+            res.send({ ...business.rows[0] });
+          }).catch((err) => console.log(5555, err));
+      } else {
+        next(unauthorized('unauthorize'));
+      }
+    })
+      .catch((err) => console.log(99, err));
+  })
+    .catch((err) => console.log(765342, err));
+};
