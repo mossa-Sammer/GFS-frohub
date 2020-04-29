0949640a72394cda32aea091591dd694845a52cd
Angham116 <angham.1161995@hotmail.com>
2020-04-29 13:09:16 +0300

    solve js json format relates#125

--- server/__tests__/database/addBusiness.test.js
+++ server/__tests__/database/addBusiness.test.js
@@ -17,7 +17,15 @@ describe('Add stylist business details', () => {
       sortCode: '08-99-99',
       preferredPayMethod: 'card',
     };
-    const businessFields = ['business_id', 'user_id', 'full_name', 'account_number', 'sort_code', 'preferred_pay_method'];
+
+    const businessFields = [
+      'business_id',
+      'user_id', 'full_name',
+      'account_number',
+      'sort_code',
+      'preferred_pay_method',
+    ];
+
     const stylist = await getStylistUser();
     const { user_id: userID } = stylist.rows[0];
     const insertedBusiness = await insertStylistBusiness(userID, stylistBusiness);
