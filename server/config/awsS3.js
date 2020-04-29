577964e9fb6c711c0b453ee3c8398e361ef45282
mossa-Sammer <mossa708496@gmail.com>
2020-04-29 17:42:08 +0300

    configured aws credintials relates #152

---
+++ server/config/awsS3.js
@@ -0,0 +1,9 @@
+const AWS = require('aws-sdk');
+
+const S3 = new AWS.S3({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: 'eu-west-2',
+});
+
+module.exports = S3;
