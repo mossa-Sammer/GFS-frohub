ac90c884942669a6afa89d422e3c16af59d4caed
Angham116 <angham.1161995@hotmail.com>
2020-04-30 17:09:23 +0300

    GET /api/services/lengthes testing /api/services/lengthes #159

---
+++ server/__tests__/api/getAllServicesLengthes.test.js
@@ -0,0 +1,25 @@
+const supertest = require('supertest');
+const app = require('../../app');
+const dbConnection = require('../../database/config/dbConnection');
+const dbBuild = require('../../database/config/dbBuild');
+
+beforeAll(() => dbBuild());
+afterAll(() => dbConnection.end());
+test('GET /api/services/lengthes', async (done) => {
+  const serviceLengthesFields = [
+    'service_length_id',
+    'name',
+    'status',
+  ];
+  try {
+    const response = await supertest(app)
+      .get('/api/services/lengthes')
+      .expect(200)
+      .expect('Content-Type', /json/);
+    const responseFields = Object.keys(response.body.data[0]);
+    expect(responseFields).toEqual(serviceLengthesFields);
+    done();
+  } catch (err) {
+    done(err);
+  }
+});
--- server/routes/index.js
+++ server/routes/index.js
@@ -43,7 +43,7 @@ router.delete('/salon/:id/service/:id', services.deleteSalonService);
 // Service
 router.get('/services', services.getAllServices);
 router.get('/service/:id', services.getService);
-router.get('/service/lengthes', services.getServicesLengthes);
+router.get('/services/lengthes', services.getServicesLengthes);
 router.post('/service', services.addService);
 router.patch('/service/:id', services.updateService);
 router.delete('/service', services.deleteService);
