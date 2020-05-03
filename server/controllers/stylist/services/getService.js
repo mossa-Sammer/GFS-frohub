b51655cb5b2763758eda50915d02ae32bf823a35
Angham116 <angham.1161995@hotmail.com>
2020-05-03 07:29:57 +0300

    refactor: get service by id: return res.json instead of res.send #158

--- server/controllers/stylist/services/getService.js
+++ server/controllers/stylist/services/getService.js
@@ -6,7 +6,7 @@ module.exports = async (req, res, next) => {
   const { id } = req.params;
   try {
     const service = await getService(id);
-    res.send({ service: service.rows });
+    res.json({ service: service.rows });
   } catch (err) {
     next(err);
   }
