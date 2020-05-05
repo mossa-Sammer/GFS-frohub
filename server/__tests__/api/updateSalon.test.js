0d9b4aa4d51a372bfe4bf8e5201493fee3886f30
mossa-Sammer <mossa708496@gmail.com>
2020-05-05 13:22:17 +0300

    used dynamic data relates #170

--- server/__tests__/api/updateSalon.test.js
+++ server/__tests__/api/updateSalon.test.js
@@ -10,43 +10,45 @@ afterAll(() => connection.end());
 const getFirstSalon = () => connection.query('SELECT * FROM salon LIMIT 1');
 
 test('API PATCH /api/salon', async (done) => {
-  const data = {
-    salon: {
-      salonId: 1,
-      name: 'update salon',
-      about: 'lorem lorem lorem',
-      profileImage: 'pro',
-      coverImage: 'cover',
-      document: 'ddd',
-      type: 'mobile',
-      street: 'omar el mukhtar',
-      city: 'gaza',
-      countryCode: 'PS',
-      postalCode: '00972',
-    },
-    openingTimes: [
-      { day: 1, fromTime: '00:00', toTime: '09:15' },
-      { day: 2, fromTime: '03:00', toTime: '10:15' },
-      { day: 3, fromTime: '02:00', toTime: '11:15' },
-      { day: 4, fromTime: '01:00', toTime: '12:15' },
-    ],
-    zones: [
-      {
-        fromZone: 1,
-        toZone: 2,
-        price: 100,
-      },
-      {
-        fromZone: 4,
-        toZone: 6,
-        price: 200,
-      },
-    ],
-  };
-  const expectedFields = ['salon', 'zones', 'openingTimes'];
   try {
     const { rows: [firstSalon] } = await getFirstSalon();
     const { salon_id: salonId } = firstSalon;
+    const data = {
+      salon: {
+        salonId,
+        name: 'update salon',
+        about: 'lorem lorem lorem',
+        profileImage: 'pro',
+        coverImage: 'cover',
+        document: 'ddd',
+        type: 'mobile',
+        street: 'omar el mukhtar',
+        city: 'gaza',
+        countryCode: 'PS',
+        postalCode: '00972',
+      },
+      openingTimes: [
+        { day: 1, fromTime: '00:00', toTime: '09:15' },
+        { day: 2, fromTime: '03:00', toTime: '10:15' },
+        { day: 3, fromTime: '02:00', toTime: '11:15' },
+        { day: 4, fromTime: '01:00', toTime: '12:15' },
+      ],
+      zones: [
+        {
+          fromZone: 1,
+          toZone: 2,
+          price: 100,
+        },
+        {
+          fromZone: 4,
+          toZone: 6,
+          price: 200,
+        },
+      ],
+    };
+
+    const expectedFields = ['salon', 'zones', 'openingTimes'];
+
     const res = await request(app)
       .patch(`/api/salon/${salonId}`)
       .send(data)
