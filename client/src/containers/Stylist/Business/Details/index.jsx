abdcfa888881d5da18121eb4b2a2891896bab93f
Angham116 <angham.1161995@hotmail.com>
2020-05-07 11:13:13 +0300

    add business details and finance file structure #180

---
+++ client/src/containers/Stylist/Business/Details/index.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+import './style.css';
+import './media.css';
+
+export default class BusinessDetails extends Component {
+  render() {
+    return <div>BusinessDetails</div>;
+  }
+}
