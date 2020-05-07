db7db32405553edc99f566a2a0cefb723dea76fd
Angham116 <angham.1161995@hotmail.com>
2020-05-07 11:32:58 +0300

    add business details and finance tabs #180

--- client/src/containers/Stylist/Business/index.jsx
+++ client/src/containers/Stylist/Business/index.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
-import { Button } from 'antd';
+import { Button, Tabs } from 'antd';
 import { Link, withRouter } from 'react-router-dom';
 
+import BusinessDetails from './Details';
+import Finance from './Finance';
+
 import { SALON_URL } from '../../../routes_urls';
+
+import './style.css';
+
+const { TabPane } = Tabs;
 
 class Business extends Component {
   render() {
     return (
-      <div>
-        <div>business</div>
-        <Button>
+      <div className="business-container">
+        <Tabs defaultActiveKey="details" className="tabs__business-container">
+          <TabPane tab="Business Details" key="details">
+            <BusinessDetails />
+          </TabPane>
+          <TabPane tab="Finance" key="finance">
+            <Finance />
+          </TabPane>
+        </Tabs>
+        <Button className="business__next-btn">
           <Link to={SALON_URL}>Next</Link>
         </Button>
       </div>
