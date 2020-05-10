96af3f9ece8f54408ac6f2cda937bbf98561fd49
mossa-Sammer <mossa708496@gmail.com>
2020-05-10 15:20:43 +0300

    provided strongest validation and fetched user data realates #182

--- client/src/containers/Stylist/Personal/index.js
+++ client/src/containers/Stylist/Personal/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { Button, Form, Input, Select } from 'antd';
 
+import axios from 'axios';
+
 import countries from 'country-data';
 
 import { BUSINESS_URL } from '../../../routes_urls';
@@ -10,6 +12,32 @@ import './style.css';
 const { Option } = Select;
 
 class PersonalForm extends Component {
+  state = {
+    // userId: '',
+    email: '',
+    firstName: '',
+    lastName: '',
+    phoneNumber: '',
+    // role: '',
+    changed: false,
+    error: false,
+  };
+
+  async componentDidMount() {
+    const { data } = await axios.get('/api/user/2/personal');
+    const {
+      email,
+      role,
+      first_name: firstName,
+      last_name: lastName,
+      phone_number: phoneNumber,
+      user_id: userId,
+    } = data;
+
+    const userData = { userId, email, firstName, lastName, phoneNumber, role };
+    this.setState({ ...userData });
+  }
+
   handleSubmit = e => {
     e.preventDefault();
     const { form, history } = this.props;
@@ -21,11 +49,35 @@ class PersonalForm extends Component {
     });
   };
 
+  handleFormChange = () => {
+    const { changed } = this.state;
+
+    const {
+      form: { getFieldsError },
+    } = this.props;
+
+    const isError = Object.values(getFieldsError()).some(e => e !== undefined);
+    this.setState({ error: isError });
+
+    if (!changed) this.setState({ changed: true });
+  };
+
   render() {
+    const {
+      email,
+      firstName,
+      lastName,
+      phoneNumber,
+      changed,
+      error,
+    } = this.state;
+
     const {
       form: { getFieldDecorator },
     } = this.props;
+
     const { all: codes } = countries.callingCodes;
+
     const prefixSelector = getFieldDecorator('prefix', {
       initialValue: '+44',
     })(
@@ -47,9 +99,14 @@ class PersonalForm extends Component {
     return (
       <div>
         <h3>Personal details</h3>
-        <Form className="personal-form" onSubmit={this.handleSubmit}>
+        <Form
+          className="personal-form"
+          onChange={this.handleFormChange}
+          onSubmit={this.handleSubmit}
+        >
           <Form.Item className="email-field" label="Email">
             {getFieldDecorator('email', {
+              initialValue: email,
               rules: [
                 {
                   type: 'email',
@@ -64,9 +121,14 @@ class PersonalForm extends Component {
           </Form.Item>
           <Form.Item className="phone-field" label="Phone Number">
             {getFieldDecorator('phone-field', {
+              initialValue: phoneNumber,
               rules: [
                 {
                   required: true,
+                  message: 'Please input your phone number!',
+                },
+                {
+                  whitespace: true,
                   message: 'Please input your phone number!',
                 },
               ],
@@ -75,9 +137,14 @@ class PersonalForm extends Component {
           <div className="name-wrapper">
             <Form.Item className="first-name-field" label="First Name">
               {getFieldDecorator('firstName', {
+                initialValue: firstName,
                 rules: [
                   {
                     required: true,
+                    message: 'Please input your first name',
+                  },
+                  {
+                    whitespace: true,
                     message: 'Please input your first name',
                   },
                 ],
@@ -85,9 +152,14 @@ class PersonalForm extends Component {
             </Form.Item>
             <Form.Item className="last-name-field" label="Last Name">
               {getFieldDecorator('lastName', {
+                initialValue: lastName,
                 rules: [
                   {
                     required: true,
+                    message: 'Please input your last name',
+                  },
+                  {
+                    whitespace: true,
                     message: 'Please input your last name',
                   },
                 ],
@@ -100,8 +172,9 @@ class PersonalForm extends Component {
               className="personal-next-btn"
               type="primary"
               htmlType="submit"
+              disabled={error}
             >
-              Next
+              {changed ? 'Save & Next' : 'Next'}
             </Button>
           </Form.Item>
         </Form>
