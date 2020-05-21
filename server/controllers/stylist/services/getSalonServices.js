f8a8787c52b679e02eb3eb69d27f6d0f4f32a5b2
Angham116 <angham.1161995@hotmail.com>
2020-05-21 12:20:32 +0300

    get salon services api

--- server/controllers/stylist/services/getSalonServices.js
+++ server/controllers/stylist/services/getSalonServices.js
@@ -1,3 +1,16 @@
-const { notImplemented } = require('@hapi/boom');
-// eslint-disable-next-line no-unused-vars
-module.exports = (req, res, next) => next(notImplemented('not implemented'));
+const dbConnection = require('../../../database/config/dbConnection');
+
+const getSalonServices = async (id) => dbConnection.query(
+  'SELECT * FROM salon_service inner join service on salon_service.service_id = service.service_id inner join service_length on salon_service.service_length_id = service_length.service_length_id WHERE salon_service.salon_id=$1', [id],
+);
+
+module.exports = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const services = await getSalonServices(id);
+    const { rows: salonServices } = services;
+    res.json({ salonServices });
+  } catch (err) {
+    next(err);
+  }
+};
