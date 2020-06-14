99b8185e670fb677bfa411521e90414a574505b1
mossa-Sammer <mossa708496@gmail.com>
2020-06-14 10:06:26 +0300

    managed salon state and splited api calls relates #183

---
+++ client/src/containers/Stylist/Salon/api.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+// get signedUrl
+
+const getSignedUrl = (userId, type) => {
+  return axios.post(`/api/upload/${userId}`, {
+    contentTypes: [type],
+  });
+};
+
+const uploadFiles = (urls, files) => {
+  return urls.map((url, index) => {
+    return axios.put(url, files[index], {
+      headers: {
+        'Content-Type': files[index].type,
+      },
+    });
+  });
+};
+
+const addSalon = (userId, salon, openingTimes, zones) => {
+  return axios.post(`/api/salon/${userId}`, {
+    salon,
+    openingTimes,
+    zones,
+  });
+};
+
+const updateSalon = (salonId, salon, openingTimes, zones) => {
+  return axios.patch(`/api/salon/${salonId}`, {
+    salon,
+    openingTimes,
+    zones,
+  });
+};
+
+export { getSignedUrl, uploadFiles, addSalon, updateSalon };
