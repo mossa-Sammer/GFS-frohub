a7e05bffb2896c8ba956f472cb8e89488dd8b3e3
Angham116 <angham.1161995@hotmail.com>
2020-06-17 14:57:23 +0300

    refactor auth with store user data in local storage

--- client/src/auth/auth.action.js
+++ client/src/auth/auth.action.js
@@ -5,11 +5,12 @@ export const AUTHENTICANTE_FAIL = 'AUTHENTICANTE_FAIL';
 const checkAuth = () => {
   return async dispatch => {
     try {
-      if (localStorage.getItem('user'))
+      if (localStorage.getItem('user')) {
         dispatch({
           type: AUTHENTICANTE_SUCCESS,
+          payload: JSON.parse(localStorage.getItem('user')),
         });
-      else
+      } else
         dispatch({
           type: AUTHENTICANTE_FAIL,
         });
--- client/src/containers/Login/login.reducer.js
+++ client/src/containers/Login/login.reducer.js
@@ -1,29 +1,25 @@
-import { LOGIN_ERROR, LOGIN_LOADING } from './login.action';
+import { LOGIN_ERROR, LOGIN_LOADING, LOGIN_SUCCESS } from './login.action';
 import { AUTHENTICANTE_SUCCESS } from '../../auth/auth.action';
-
-const storedUser = JSON.parse(localStorage.getItem('user'));
 
 const initState = {
   error: null,
   loading: false,
-  loggedUser: storedUser || {},
+  loggedUser: {},
 };
 
 export default function loginReducer(state = initState, action) {
-  let user;
   switch (action.type) {
+    case LOGIN_SUCCESS: {
+      return {
+        ...state,
+        loggedUser: action.payload.loggedUser,
+      };
+    }
     case AUTHENTICANTE_SUCCESS:
-      if (Object.keys(initState.loggedUser) !== 0) {
-        user = initState.loggedUser;
-      } else {
-        const { loggedUser } = action.payload;
-        user = loggedUser;
-      }
       return {
         ...state,
         error: null,
         loading: false,
-        loggedUser: user,
       };
     case LOGIN_LOADING:
       return {
