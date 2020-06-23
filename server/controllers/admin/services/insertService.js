0e2aec7d01df1ad62c481b07fe6b92f04e1a6637
mossa-sammer <mossa708496@gmail.com>
2020-06-23 11:49:59 +0300

    made the isert service by admin route relates #230

--- server/controllers/admin/services/insertService.js
+++ server/controllers/admin/services/insertService.js
@@ -1,3 +1,24 @@
-const { notImplemented } = require('@hapi/boom');
+const boom = require('@hapi/boom');
+const { validationError } = require('../../helper');
+const { addSalonService: addSalonServiceValidation } = require('./validation');
 
-module.exports = async (req, res, next) => next(notImplemented());
+const { insertServiceByAdmin } = require('../../../database/queries');
+
+
+module.exports = async (req, res, next) => {
+  try {
+    const { name, status } = req.body;
+    const validatedService = await addSalonServiceValidation.validate({ name, status });
+    const {
+      rows: [addedService],
+    } = await insertServiceByAdmin(validatedService.name, validatedService.status);
+    res.json({ msg: 'service added successfully', data: addedService });
+  } catch (e) {
+    if (e.name === 'ValidationError') {
+      const errors = validationError(e);
+      const errObj = boom.badData('message', errors);
+      next(errObj);
+    }
+    next(e);
+  }
+};
