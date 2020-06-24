e832c5c097e037df7c2d135c58e215d4308ebd6d
Angham116 <angham.1161995@hotmail.com>
2020-06-24 12:28:01 +0300

    fix typo

--- server/database/queries/admin/partner/getAllPartners.js
+++ server/database/queries/admin/partner/getAllPartners.js
@@ -1,5 +1,5 @@
 const dbConnection = require('../../../config/dbConnection');
 
 module.exports = () => dbConnection.query(
-  'SELECT user_id, first_name || last_name AS fullNAme , email, calling_code, phone_number, role FROM "user"',
+  'SELECT user_id, first_name || last_name AS fullName , email, calling_code, phone_number, role FROM "user"',
 );
