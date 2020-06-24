bd0f43b14a78c378105df15bd6a3d6d46a633eab
mossa-sammer <mossa708496@gmail.com>
2020-06-24 15:26:26 +0300

    made a model component to add new service type relates #236

---
+++ client/src/components/ServiceTypeForm/index.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+import { Modal, Button, Form, Input, Radio } from 'antd';
+
+import './style.css';
+
+class ServiceTypeForm extends Component {
+  state = {
+    visible: false,
+    confirmLoading: false,
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  handleOk = async () => {
+    const { handleSubmit } = this.props;
+
+    this.setState({
+      confirmLoading: true,
+    });
+
+    await handleSubmit();
+
+    this.setState({
+      visible: false,
+      confirmLoading: false,
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    const { name, status, handleName, handleStatus } = this.props;
+    const { visible, confirmLoading } = this.state;
+    return (
+      <div>
+        <Button
+          type="primary"
+          className="add-new-service"
+          onClick={this.showModal}
+        >
+          add a new service
+        </Button>
+        <Modal
+          title="New Service"
+          visible={visible}
+          onOk={this.handleOk}
+          confirmLoading={confirmLoading}
+          onCancel={this.handleCancel}
+        >
+          <div>
+            <label htmlFor="serviceName" className="name-label">
+              Name:
+              <Input
+                aria-label="serviceName"
+                type="text"
+                onChange={handleName}
+                value={name}
+              />
+            </label>
+          </div>
+          <div className="service-status">
+            <label className="status-label">Status:</label>
+            <Radio.Group onChange={handleStatus} value={status}>
+              <Radio value="active">active</Radio>
+              <Radio value="inactive">inactive</Radio>
+            </Radio.Group>
+          </div>
+        </Modal>
+      </div>
+    );
+  }
+}
+export default ServiceTypeForm;
