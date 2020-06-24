24c14fc7af26933d11a4b7b61c2dce18481069e2
Angham116 <angham.1161995@hotmail.com>
2020-06-24 13:28:49 +0300

    frontend: get all users by admin #235

--- client/src/containers/Admin/Partners/index.js
+++ client/src/containers/Admin/Partners/index.js
@@ -1,7 +1,46 @@
+/* eslint-disable no-nested-ternary */
 import React, { Component } from 'react';
 
+import { message, Button } from 'antd';
+
+import getAllPartners from './api';
+
+import { Loading } from '../../../components';
+
+import './style.css';
+import './media.css';
+
 export default class Partners extends Component {
+  state = {
+    loading: true,
+    users: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const users = await getAllPartners();
+      this.setState({ users, loading: false });
+    } catch (err) {
+      message.error(err.response.data.message);
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
-    return <div>Partners</div>;
+    const { loading, users } = this.state;
+    return (
+      <>
+        <div className="add__new__user-container">
+          <Button className="add__new__user-btn">Add New User</Button>
+        </div>
+        {loading ? (
+          <Loading />
+        ) : !users.length ? (
+          <p>There is no users</p>
+        ) : (
+          <div>{console.log(88888888888, users)}</div>
+        )}
+      </>
+    );
   }
 }
