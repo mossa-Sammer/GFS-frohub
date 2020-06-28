d30a5b7d48b7a2fafa1d27b5aec61166b967d8c8
mossa-sammer <mossa708496@gmail.com>
2020-06-28 11:21:28 +0300

    made the update and delete service queires relates #246

--- server/database/queries/admin/services/index.js
+++ server/database/queries/admin/services/index.js
@@ -1,9 +1,11 @@
 const getAllServices = require('./getAllServices');
 const insertService = require('./insertService');
 const updateService = require('./updateService');
+const deleteService = require('./deleteService');
 
 module.exports = {
   getAllServices,
   insertService,
   updateService,
+  deleteService,
 };
