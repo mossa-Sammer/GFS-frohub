11ca077728ab9d38e4e01a2bbc1f798fe81cbf2d
mossa-sammer <mossa708496@gmail.com>
2020-07-02 12:08:45 +0300

    arranged days

--- client/src/components/DaySelect/index.js
+++ client/src/components/DaySelect/index.js
@@ -6,12 +6,12 @@ const { Option } = Select;
 
 const ukDays = [
   { index: 1, name: 'Monday' },
-  { index: 0, name: 'Sunday' },
   { index: 2, name: 'Tuesday' },
   { index: 3, name: 'Wednesday' },
   { index: 4, name: 'Thursday' },
   { index: 5, name: 'Friday' },
   { index: 6, name: 'Saturday' },
+  { index: 0, name: 'Sunday' },
 ];
 
 const days = [
