bc110bc14fb7f9935bf7d680fb1988c2a2146bf1
mossa-sammer <mossa708496@gmail.com>
2020-07-02 14:34:14 +0300

    fixed bugs

--- client/src/components/Uploader/index.js
+++ client/src/components/Uploader/index.js
@@ -13,11 +13,11 @@ function getBase64(img, callback) {
 function beforeUpload(file) {
   const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
   if (!isJpgOrPng) {
-    message.error('You can only upload JPG/PNG file!');
+    message.error('You can only upload JPG/PNG file.');
   }
   const isLt2M = file.size / 1024 / 1024 < 2;
   if (!isLt2M) {
-    message.error('Image must smaller than 2MB!');
+    message.error('Please upload an image smaller than 2 MB.');
   }
   return isJpgOrPng && isLt2M;
 }
